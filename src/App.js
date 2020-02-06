a815cd26b8e19668af7a4291b4044036abeb2d5f
Quinn Krug <quinn.krug@gmail.com>
2020-02-06 16:28:34 -0700

    add unique key to dummy data

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: '1',name: 'Max', age: 28 },
+      { id: '2',name: 'Manu', age: 29 },
+      { id: '3',name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -45,7 +45,8 @@ class App extends Component {
             return <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
-              age={person.age} />
+              age={person.age}
+              key={person.id} />
           })}
         </div>
 
