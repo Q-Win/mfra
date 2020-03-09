9768bc14e8bc429d912bed2856bc210dd5f8c3a2
Quinn Krug <quinn.krug@gmail.com>
2020-03-09 16:51:13 -0600

    add styled component button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
-import './App.css';
+import styled from 'styled-components';
 
+import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -94,9 +108,9 @@ class App extends Component {
         <div className="App">
           <h1>Hi, Im a React App</h1>
           <p className={classes.join( ' ' )}>This is really working!</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          <StyledButton onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </StyledButton>
           {persons}
         </div>
     );
